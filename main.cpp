837e24320135be26ac6fd3d098006effc167303c
luca-trifan <luca.tc100@gmail.com>
2022-04-21 11:20:47 +0300

    fisierele principale

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin("harta.in");
+
+int n, m, harta[100][100]
+
+struct statie
+{
+    int x,y;
+};
+
+struct linie
+{
+    int nr;
+    statie traseu[100];
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
